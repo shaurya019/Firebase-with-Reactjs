568ca1cbdec5a680ebaff94753796c77c1377404
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-01-29 22:13:24 +0530

    “second”

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import {getDatabase,ref,set} from "firebase/database"; 
 import {app} from "./firebase"; 
-import './App.css';
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const db = getDatabase(app);
 
 function App() {
-  const  putdata = () => {
-    set(ref(db,"users/shaurya"),{
-      id:1,
-      name:"Shaurya",
-      age:22
-    })
+  // const  putdata = () => {
+  //   set(ref(db,"users/shaurya"),{
+  //     id:1,
+  //     name:"Shaurya",
+  //     age:22
+  //   })
+  // }
+
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  const auth = getAuth();
+  const createUser = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    alert("Success");
+    console.log(userCredential);
+  })
+  .catch((error) => {
+    alert("Error");
+    console.log(error.value);
+  });
   }
   return (
     <div className="App">
-      <h1>Firebase</h1>
+      <h1>Firebase Auth</h1>
+      <input onChange={(e) => {
+        setEmail(e.target.value)
+      }} value={email} type="email" reuired placeholder="Enter email" /><label>Email</label>
+      <input onChange={
+        (e) => {
+          setPassword(e.target.value)
+        }
+      } value={password} type="password" reuired placeholder="Enter password" /><label>Password</label>
       <button
-      onClick={putdata}
+      onClick={createUser}
       >
-        Click it
+        Submit
       </button>
     </div>
   );
